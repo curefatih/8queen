c25e19218315cebc36ca093fa79d8ce620f2a40f
curefatih <curefat@gmail.com>
2020-03-11 19:47:18 +0300

    Update cross check

--- app.py
+++ app.py
@@ -23,45 +23,44 @@ MAX_X_CORD = 8
 MAX_Y_CORD = 8
 
 
-# vezirin çarprazlarının listesi bulur
-def get_cross_list(x, y):
-    def right_top(a, b): return {x: a+1, y: b+1}
-    def left_top(a, b): return {x: a-1, y: b+1}
-    def right_bottom(a, b): return {x: a+1, y: b-1}
-    def left_bottom(a, b): return {x: a-1, y: b-1}
-
-    check_list = {}
-
-    # TODO çakışmları kümeye ekle sonrasında cevap olarak kümeyi döndür
-
-    return check_list
-
-
 # kontrol fonksiyonları
 
-# yatayda kontrol et
-
-
+# yatayda çakışma sayısını verir
 def horizantal_check(arr):
+    """
+    arr: [int]
+    """
     count = 0
     for index in range(len(arr) - 1):
-        print("index", index)
+        # print("index", index)
         for t in range(index + 1, len(arr)):
-            print("t: ", t)
+            # print("t: ", t)
             if(arr[index] == arr[t]):
                 count = count + 1
     return count
 
 # dikeyde kontrol edilebilmesi için matris kullanışmış olması lazım. Bizim problemimizde bununla ilgilenmiyoruz. Vezirleri sütunlara dağıtıldığı varsayılıyor
-
-
 def vertical_check():
+    """
+    arr: [int]
+    """
     return 0
 
-
+# vezirin çarprazlarının sayısını bulur
 def cross_check(arr):
+    """
+     arr: [int]
+    """
+    total_conflict = 0
+    # her bir sütun için kontrol ediyoruz
+    for index in range(len(arr) - 1):
+        for check in range(index + 1, len(arr)):
+            # iki index arasındaki fark
+            distance = check - index
+            # print("index ", index, " - current ", check, " --- distance",
+            #       distance, "---plus : ", arr[index] + distance, " eq: ", arr[check])
+            if(arr[index] + distance == arr[check] or arr[index] - distance == arr[check]):
+                total_conflict = total_conflict + 1  # çakışma varsa sayıyı bir artır
+        # print("_______________")
 
-    for x in range(len(arr)):
-        c_check_list = get_cross_list(x, arr[x])
-
-    return len(c_check_list)
+    return total_conflict
